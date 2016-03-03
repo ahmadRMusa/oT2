210ecee997dd2b1671563b698639e1e8eaeacb2e
ejb <mail@elliotbentley.com>
2016-03-03 10:25:08 -0500

    Integrate CodeMirror editor

--- src/controllers/transcript.js
+++ src/controllers/transcript.js
@@ -1,36 +1,47 @@
 let Ractive = require('ractive');
-
+let prosemirror = require("prosemirror");
 let template = require('raw!../templates/transcript.html');
 
 let model = {
-    _text: "",
-    transcript: {
-        date: +(new Date()),
-        lastMedia: "",
-        offsetSeconds: 0,
-        history: []
+    date: +(new Date()),
+    lastMedia: "",
+    offsetSeconds: 0,
+    history: [],
+    text: '',
+    _userEditing: false,
+    cleanText: function(){
+        // TODO: filter out non-whitelisted HTML
+        return this.get('text');
     }
 };
 
-let computed = {
-    'transcript:text': {
-        get: function(){
-            // TODO: filter HTML out
-            return this._text;
-        },
-        set: function(text){
-            this._text = text;
-        }
-    }
-};
-
-let transcriptController = new Ractive({
+let controller = new Ractive({
     el: document.querySelector('.transcript-container'),
     template: template,
-    data: model,
-    computed: computed
+    data: model
 });
 
-export {transcriptController as transcriptController};
+let editor = new prosemirror.ProseMirror({
+    place: controller.find('.text-editor'),
+    docFormat: 'html',
+    doc: ''
+});
+
+// when user types something update the model
+editor.on('change',function(){
+    controller.set('_userEditing',true);
+    controller.set('text', editor.getContent('html'));
+    controller.set('_userEditing',false);
+});
+
+// when text in model is set programmatically, update editor
+controller.observe('text', function(newValue, oldValue){
+    if ( !controller.get('_userEditing') ) {
+        editor.setContent(newValue, 'html');
+    }
+});
+
+
+export {controller as transcriptController};
 
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ let localforage = require('localforage');
 
 import {transcriptController} from './controllers/transcript';
 
-transcriptController.set('transcript.text','xx');
+transcriptController.set('text','xx');
