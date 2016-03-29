e11a6ba2b7c75c3d2a1ef5b682a3414209f75338
ejb <mail@elliotbentley.com>
2016-03-29 16:26:27 +0100

    Select file

--- src/controllers/storage.js
+++ src/controllers/storage.js
@@ -8,6 +8,7 @@ function createController(opts){
     let storage = Storage();
     
     let model = {
+        activeFile: -1
     };
 
     let controller = new Ractive({
@@ -16,9 +17,18 @@ function createController(opts){
         data: model
     });
     
+    controller.on('selectFile',(e)=>{
+        let id = e.node.dataList.id;
+        controller.set('activeFile',id);
+    });
+    
     storage.list().then((files)=>{
+        let activeFile = controller.get('activeFile');
         files.forEach((f)=>{
             f.dateFormatted = Date(f);
+            if (f.id === activeFile) {
+                f.active = true;
+            }
         })
         controller.set('files',files);
     });
